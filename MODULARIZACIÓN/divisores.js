85d226049879b8775874b0b012e69ec2f4a07207
Candela-Araneta <aranetacandela@gmail.com>
2023-04-27 08:13:58 -0300

    27/04/23

--- MODULARIZACIÓN/divisores.js
+++ MODULARIZACIÓN/divisores.js
@@ -1,29 +1,24 @@
+"use strict";
 // Implemente un método llamado cantidadDeDivisores que reciba un número entero y devuelva la
 // cantidad de divisores.
 // Por ejemplo, para el número 16, sus divisores son 1,2,4,8 y 16 por lo que la respuesta
 // debería ser 5.
 // Re-utilice el método esMultiplo implementado para el ejercicio anterior. //
-
-import * as rls from "readline-sync";
-
+Object.defineProperty(exports, "__esModule", { value: true });
+var rls = require("readline-sync");
 //Método utilizado en el ejercicio anterior.
-function esMultiplo(dividendo:number, divisor:number) : boolean {
+function esMultiplo(dividendo, divisor) {
     return ((dividendo % divisor) == 0);
 }
-
-
-function cantidadDeDivisores(nro: number) : number {
-    let i: number;
-    let cantidad: number = 0;
-
+function cantidadDeDivisores(nro) {
+    var i;
+    var cantidad = 0;
     for (i = 1; i <= nro; i++) {
-        if (esMultiplo(nro,i)) {
+        if (esMultiplo(nro, i)) {
             cantidad = cantidad + 1;
         }
     }
-return cantidad;
+    return cantidad;
 }
-
-let numero : number = rls.questionInt("Ingrese un Numero: ");
-console.log("La cantidad de divisores del Numero",numero, "es",cantidadDeDivisores(numero));
-
+var numero = rls.questionInt("Ingrese un Numero: ");
+console.log("La cantidad de divisores del Numero", numero, "es", cantidadDeDivisores(numero));
--- MODULARIZACIÓN/multiplos.js
+++ MODULARIZACIÓN/multiplos.js
@@ -1,19 +1,18 @@
+"use strict";
 // Cree un método esMultiplo con 2 parámetros que devuelva el valor lógico verdadero o falso
 // según si el primer número que se indique como parámetro es múltiplo del segundo.
 // Recuerde que un número es múltiplo de otro si al dividirlo su resto es 0.
 // Recuerde que la operación mod permite saber si el resto d euna división es 0. //
-
-import * as rls from "readline-sync";
-
-function esMultiplo(dividendo:number, divisor:number): boolean {
+Object.defineProperty(exports, "__esModule", { value: true });
+var rls = require("readline-sync");
+function esMultiplo(dividendo, divisor) {
     return ((dividendo % divisor) == 0);
 }
-
-let num1:number = rls.questionInt("Ingrese primer número:");
-let num2:number = rls.questionInt("Ingrese segundo número:");
-
+var num1 = rls.questionInt("Ingrese primer número:");
+var num2 = rls.questionInt("Ingrese segundo número:");
 if (esMultiplo(num1, num2)) {
-    console.log("El Numero",num1, "ES multiplo de",num2);
-} else {
-    console.log("El Numero",num1, "NO es multiplo de",num2);
+    console.log("El Numero", num1, "ES multiplo de", num2);
+}
+else {
+    console.log("El Numero", num1, "NO es multiplo de", num2);
 }
